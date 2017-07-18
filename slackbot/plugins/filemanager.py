398bd23d0c954822e0513e675ff59a639849a400
wertherzhang <wertherzhang@china-liantong.com>
2017-07-18 16:52:11 +0800

    add filemanager

---
+++ slackbot/plugins/filemanager.py
@@ -0,0 +1,69 @@
+import os
+from slackbot.bot import respond_to
+from slackbot.utils import download_file, create_tmp_file
+
+
+# @respond_to(r'upload \<?(.*)\>?')
+# def upload(message, url):
+#     url = url.lstrip('<').rstrip('>')
+#     fname = os.path.basename(url)
+#     message.reply('uploading {}'.format(fname))
+#     if url.startswith('http'):
+#         with create_tmp_file() as tmpf:
+#             download_file(url, tmpf)
+#             message.channel.upload_file(fname, tmpf, 'downloaded from {}'.format(url))
+#     elif url.startswith('/'):
+#         message.channel.upload_file(fname, url)
+
+FILEHUB_ROOT = "/extdisks/sda1/fmroot/"
+
+def fm_upload(message, args):
+    url = args
+    fname = os.path.basename(url)
+    message.reply('uploading {}'.format(fname))
+    if url.startswith('http'):
+        with create_tmp_file() as tmpf:
+            download_file(url, tmpf)
+            message.channel.upload_file(fname, tmpf, 'download from {}'.format(url))
+        else:
+            message.channel.upload_file(fname, os.path.join(FILEHUB_ROOT, url))
+
+def fm_ls(message, args):
+    argv = args.split()
+    result = []
+    if len(argv) == 0:
+        argv.append(FILEHUB_ROOT);
+    for arg in argv:
+        files = os.listdir(arg)
+        message.reply('{}'.format(' '.join(files[:])))
+
+def fm_download(message, args):
+    url = args
+    fname = os.path.basename(url)
+    if url.startswith('http'):
+        message.reply('downloading {}'.format(fname))
+        with open('{}/{}'.format(FILEHUB_ROOT, fname)) as tmpf:
+            download_file(url, tmpf)
+            message.reply('download {} to {} success'.format(url, fname));
+
+
+def parse_argument(contents):
+    # fm [command] xxx
+    argv = contents.split()
+    if len(argv) > 3:
+        argv = argv[:2] + [' '.join(argv[2:])]
+    if len(argv) < 3:
+        argv = argv + [' ']
+    return argv
+
+@respond_to(r'fm [\s]*[a-zA-Z0-9]+ (.+)')
+@listen_to(r'fm [\s]*[a-zA-Z0-9]+ (.+)')
+def filemanager(message, rest):
+    contents = message.body.get('text', "")
+    _, command, args = parse_argument(contents)
+    if command == "upload":
+        fm_upload(message, args)
+    if command = "ls":
+        fm_ls(message, args)
+    if command = "download":
+        fm_download(message, args)
