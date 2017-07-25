53d53f8f8d2b6f10b5e430fb58620d49bf15e80f
wertherzhang <wertherzhang@china-liantong.com>
2017-07-25 15:58:17 +0800

    update anywhere

--- slackbot/plugins/anywhere.py
+++ slackbot/plugins/anywhere.py
@@ -14,6 +14,8 @@
 #
 #
 
+import sys
+import os
 import urllib
 import urllib2
 import cookielib
@@ -21,7 +23,10 @@ import requests
 import json
 from six.moves import _thread
 import time
-from slackbot.utils import download_file
+
+SHARED_DIR_ROOT='/extdisk/sda1/'
+DOWNLOAD_DIR='/extdisks/sda1/下载/'
+#DOWNLOAD_DIR='/home/werther/Music/'
 
 class SendAnywhere (object):
     API_ENDPOINTS = "https://send-anywhere.com"
@@ -34,11 +39,13 @@ class SendAnywhere (object):
         500:"Internal Server Error. Something went wrong on our side. We're very sorry."
     }
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, msg=None):
         self.cookie = cookielib.CookieJar()
         self.handler = urllib2.HTTPCookieProcessor(self.cookie)
         self.opener = urllib2.build_opener(self.handler)
         self.api_key = api_key
+        self.download_progress = 1.0
+        self.msg = msg
         self.__update()
 
     def __update(self):
@@ -46,32 +53,135 @@ class SendAnywhere (object):
         response = self.opener.open(request_url)
 
     def __upload_file(self, url, filename, timeout):
-        files = {'file': open(filename, 'rb')}
         try:
-            response = requests.post(url, files=files, timeout=timeout)
+            with open(filename, 'rb') as f:
+                response = requests.post(url, files={'file': f}, timeout=timeout)
         except requests.exceptions.Timeout:
             print "Timeout waiting for post"
+        print("Send file done response {}".format(response))
 
     def send(self, filename):
         key_api_url = "{}/web/v1/key".format(SendAnywhere.API_ENDPOINTS)
-        response = self.opener.open(key_api_url)
 
+        response = self.opener.open(key_api_url)
         jdata = json.loads(response.read())
         print("share key: {}".format(jdata['key']))
         #print("{}".format(jdata))
         timeout = jdata['expires_time'] - jdata['created_time']
-        _thread.start_new_thread(self.__upload_file, (jdata['weblink'], filename, timeout))
+        _thread.start_new_thread(self.__upload_file,
+                                 (jdata['weblink'], filename, timeout))
         #self.__upload_file(jdata['weblink'], filename, timeout)
-        time.sleep(timeout)
+        #time.sleep(timeout)
+        return (timeout, jdata['key'])
 
-    def receive(self, name, key):
+    def __download_callback(self, downsize, totalsize):
+        self.download_progress = float(downsize) / float(totalsize)
+
+    def __download_file(self, url, fpath, filesize, callback=None):
+        count = 0
+        blocksize = 1024*64
+        r = requests.get(url, stream=True)
+        with open(fpath, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=blocksize):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    if callback:
+                        count += 1
+                        callback(count*blocksize, filesize)
+        callback(filesize, filesize)
+        u = url + '&mode=status&_={}'.format(int(time.time()))
+        #print('status url {}'.format(u))
+        res = self.opener.open(u)
+        jdata = json.loads(res.read())
+        dirname = os.path.dirname(fpath)
+        fp = jdata.get('name', fpath)
+        #print('jdata {}'.format(jdata))
+        if fp != fpath:
+            fp = os.path.join(dirname, fp)
+            os.rename(fpath, fp)
+        if self.msg:
+            self.msg.reply("Finish downloading {} !".format(fp))
+
+    def receive(self, name, key, msg=None):
+        if 1 - self.download_progress > 0.01:
+            print("Download in progress, please wait!!")
+            return self.download_progress
+        self.msg = msg
         key_api_url = "{}/web/v1/key/{}".format(SendAnywhere.API_ENDPOINTS, key)
         response = self.opener.open(key_api_url)
         jdata = json.loads(response.read())
-        print("{}".format(jdata))
+        #print("{}".format(jdata))
+        _thread.start_new_thread(self.__download_file,
+                                 (jdata['weblink'],
+                                  name,
+                                  jdata['file_size'],
+                                  self.__download_callback))
+        return 0
 
 
 if __name__ == '__main__':
     s = SendAnywhere("")
-    s.send('Lonely.mp3')
-    #s.receive(116133)
+    #timeout, key = s.send('Lonely.mp3')
+    # print("shared key : {}".format(key))
+    # time.sleep(timeout)
+    s.receive('mp3', 156724)
+    sys.exit(1)
+
+
+from slackbot.bot import plugin_init
+from slackbot.bot import respond_to
+
+_enable = False
+_anywhere = None
+@plugin_init
+def init_anywhere(config):
+    global _enable
+    global _anywhere
+    api_key = config.get('api', '')
+    _enable = config.get('enable', False)
+    _anywhere = SendAnywhere(api_key)
+
+@respond_to(r'anywhere (.*)')
+def anywhere_command(message, rest):
+    global _enable
+    global _anywhere
+    if not _enable:
+        message.reply("SendAnywhere not enabled")
+        return
+
+    argv = ['anywhere'] + rest.split()
+    print("{}".format(argv))
+    command = argv[1]
+    if command == 'send':
+        # anywhere send [filepath]
+        if len(argv) < 3:
+            message.reply("Usage: anywhere send  [filepath]")
+        if argv[2] == None:
+            message.reply("Usage: anywhere send  [filepath]")
+        filepath = ' '.join(argv[2:])
+        (timeout, key) = _anywhere.send(os.path.join(DOWNLOAD_DIR, filepath))
+        message.reply('Shared file key {} expired in {} seconds'.format(key, timeout))
+
+    if command == 'receive':
+        # anywhere recv key filename
+        if len(argv) < 4:
+            message.reply("Usage: anywhere recv [key] [filename]")
+        if argv[2] == None or argv[3] == None:
+            message.reply("Usage: anywhere recv [key] [filename]")
+        key = argv[2]
+        filename = ' '.join(argv[3:])
+        filename = os.path.join(DOWNLOAD_DIR, key + '_' + filename)
+        ret = _anywhere.receive(filename, key, message)
+        if ret == 0:
+            message.reply("Start downloading {} to {}".format(key, filename))
+        else:
+            message.reply("Last download not done, progress {}".format(ret))
+
+    if command == 'ls':
+        print("command ls")
+        if len(argv) == 2:
+            argv = argv + ['{}'.format(DOWNLOAD_DIR)]
+        for arg in argv[2:] :
+            files = os.listdir(arg)
+            message.reply('{}'.format('\t'.join(files[:])))
--- slackbot/plugins/filemanager.py
+++ slackbot/plugins/filemanager.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
