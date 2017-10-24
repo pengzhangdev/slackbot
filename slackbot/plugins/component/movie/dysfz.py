ecf22475464a805553cfa015c3771c760101a5c5
wertherzhang <wertherzhang@china-liantong.com>
2017-10-24 10:21:07 +0800

    dysfz.py

--- slackbot/plugins/component/movie/dysfz.py
+++ slackbot/plugins/component/movie/dysfz.py
@@ -139,7 +139,7 @@ class dysfz(object):
             try:
                 soups.append(self._create_soup(url))
             except:
-                _LOGGE.exeption("Failed to fetch {}".format(url))
+                _LOGGER.exeption("Failed to fetch {}".format(url))
         for soup in soups:
             contents += self._get_movie_list(soup)
         if len(contents) != 0:
